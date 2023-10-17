671ad96afa18f7b51e77b71e981b0c7095b3e35e
dharmik-anghan <dharmikanghan09@gmail.comm>
2023-10-17 14:38:39 +0530

    Updated Get All post

--- fastapi/socialmedia/main.py
+++ fastapi/socialmedia/main.py
@@ -2,31 +2,44 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from datetime import datetime
 import psycopg2
-
-conn = psycopg2.connect("dbname=fastapi user=postgres password=anghan")
-
-if conn == None:
-    raise HTTPException(headers="DB name or username or password is wrong",status_code=404,)
-
-cursor = conn.cursor()
-
-# class Post(BaseModel):
-#     id: int | None = None
-#     title: str
-#     content: str
-#     published: bool = True
-#     created_at: datetime | None = None
-
+from psycopg2.extras import RealDictCursor
+import time
 
 app = FastAPI()
 
-# @app.get("/", tags=["test"])
-# def home():
-#     return {"msg": "Hello World!"}
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+while True:
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="fastapi",
+            user="postgres",
+            password="anghan",
+            cursor_factory=RealDictCursor,
+        )
+        cursor = conn.cursor()
+        print("Database connection was successful")
+        break
+    except Exception as error:
+        print("Connecting to database failed")
+        print(f"Error was {error}")
+        time.sleep(2)
+
+if conn == None:
+    raise HTTPException(
+        headers="DB name or username or password is wrong",
+        status_code=404,
+    )
+
 
 @app.get("/posts", tags=["Public Post"])
 def get_posts():
-    cursor.execute("select title, content, created_at from post")
-    all_posts = cursor.fetchall()
-    print(all_posts)
-    return all_posts
+    cursor.execute(""" select * from post """)
+    posts = cursor.fetchall()
+    return {"data": posts}
