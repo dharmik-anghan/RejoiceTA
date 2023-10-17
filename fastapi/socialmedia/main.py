785a26c28c7942143c99e90635f266bd89a07107
dharmik-anghan <dharmikanghan09@gmail.comm>
2023-10-17 15:35:02 +0530

    [CRUD Opp]

--- fastapi/socialmedia/main.py
+++ fastapi/socialmedia/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response, status
 from pydantic import BaseModel
 from datetime import datetime
 import psycopg2
@@ -43,3 +43,54 @@ def get_posts():
     cursor.execute(""" select * from post """)
     posts = cursor.fetchall()
     return {"data": posts}
+
+
+@app.post("/posts", status_code=201, tags=["Create Post"])
+def create_post(post: Post):
+    cursor.execute(
+        """ INSERT INTO post (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
+        (post.title, post.content, post.published),
+    )
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data": new_post}
+
+
+@app.get("/posts/{id}", tags=["Get Individual Post"])
+def get_post(id: int):
+    cursor.execute(""" SELECT * FROM post where id = %s """, (str(id),))
+    post = cursor.fetchone()
+    if not post:
+        raise HTTPException(status_code=404, detail=f"post with id {id} was not found")
+    return {"post detail": post}
+
+
+@app.delete("/posts/{id}", tags=["Delete Post"], status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    cursor.execute(""" DELETE FROM post WHERE id = %s RETURNING *""", (str(id),))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+
+    if deleted_post == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id {id} was not found",
+        )
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    cursor.execute(
+        """ UPDATE post SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
+        (post.title, post.content, post.published, str(id)),
+    )
+    updated_post = cursor.fetchone()
+    conn.commit()
+    if updated_post == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id {id} was not found",
+        )
+    return {"data": updated_post}
