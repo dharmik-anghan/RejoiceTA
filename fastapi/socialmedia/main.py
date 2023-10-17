d7cf1644e19c11d2915c8e1fa685130759547471
dharmik-anghan <dharmikanghan09@gmail.comm>
2023-10-17 11:23:28 +0530

    [Dharmik- added get all post path]

--- fastapi/socialmedia/main.py
+++ fastapi/socialmedia/main.py
@@ -1,7 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from datetime import datetime
+import psycopg2
+
+conn = psycopg2.connect("dbname=fastapi user=postgres password=anghan")
+
+if conn == None:
+    raise HTTPException(headers="DB name or username or password is wrong",status_code=404,)
+
+cursor = conn.cursor()
+
+# class Post(BaseModel):
+#     id: int | None = None
+#     title: str
+#     content: str
+#     published: bool = True
+#     created_at: datetime | None = None
+
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"msg": "Hello World!"}
+# @app.get("/", tags=["test"])
+# def home():
+#     return {"msg": "Hello World!"}
+
+@app.get("/posts", tags=["Public Post"])
+def get_posts():
+    cursor.execute("select title, content, created_at from post")
+    all_posts = cursor.fetchall()
+    print(all_posts)
+    return all_posts
