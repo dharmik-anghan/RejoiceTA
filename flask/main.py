9dd19c406d33e0e441c4244f14ee097bba1e4901
dharmik-anghan <dharmikanghan09@gmail.com>
2023-10-31 10:31:08 +0530

    [Integrated HTML with flask]

--- flask/main.py
+++ flask/main.py
@@ -1,82 +1,41 @@
-from flask import Flask, render_template, request, url_for
+# Integrate HTML with Flask
+# HTTP verb GET and POST
+from flask import Flask, render_template, request, url_for, redirect
 from markupsafe import escape
 
+# WSGI Application
 app = Flask(__name__)
 
 
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!!!</p>"
+@app.get("/")
+def welcome():
+    return render_template("index.html")
 
 
-# @app.route("/<name>")
-# def hello(name):
-#     return f"Hello, {escape(name)}"
+@app.route("/result/<int:score>")
+def result(score):
+    res = ""
+    if score >= 50:
+        res = "PASS"
+    else:
+        res = "FAIL"
+    return render_template("result.html", result=res)
 
 
-# @app.route("/projects/")
-# def projects():
-#     return "The project page"
+# Resylt checker html page
+@app.route("/submit", methods=["POST", "GET"])
+def submit():
+    total_score = 0
 
+    if request.method == "POST":
+        science = float(request.form["science"])
+        maths = float(request.form["maths"])
+        c = float(request.form["c"])
+        datascience = float(request.form["datascience"])
 
-# @app.route("/about/")
-# def about():
-#     return "The about page"
+        total_score = (science + maths + c + datascience) / 4
 
-# # variable rules
-# @app.route("/user/<username>")
-# def show_user_profile(username):
-#     return f"User {escape(username)}"
-
-# @app.route("/post/<int:post_id>")
-# def show_post(post_id):
-#     return f"Post {post_id}"
-
-# @app.route("/path/<path:subpath>")
-# def show_subpath(subpath):
-#     return f"Subpath {subpath}"
-
-# # URL Building
-# @app.route("/")
-# def index():
-#     return 'index'
-
-# @app.route("/login")
-# def login():
-#     return 'login'
-
-# @app.route("/user/<username>")
-# def profile(username):
-#     return f"{username}'s profile"
-
-# with app.test_request_context():
-#     print(url_for('index'))
-#     print(url_for('login'))
-#     print(url_for('login', next='/'))
-#     print(url_for('profile', username="John Doe"))
-
-
-# # HTTP methods
-# @app.route("/login", methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         return 'post'
-#     if request.method == 'GET':
-#         return 'GET'
-
-# Rendering Templates
-
-@app.route("/hello")
-@app.route("/hello/<name>")
-def hello(name=None):
-    return render_template('hello.html', name=name)
-
-
-with app.test_request_context('/hello', method='POST'):
-    # now you can do something with the request until the
-    # end of the with block, such as basic assertions:
-    assert request.path == '/hello'
-    assert request.method == 'POST'
+    return redirect(url_for("result", score=total_score))
 
 
 # You can direct run file from terminal and server will run like python main.py
